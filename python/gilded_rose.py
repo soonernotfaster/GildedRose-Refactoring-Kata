9eee99980a6e1c014d4b61640cdf138fbf515664
Steven Solomon <solomon.steven.m@gmail.com>
2025-07-30 19:41:03 +0000

    Reverse conditions

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -37,16 +37,17 @@ class GildedRose(object):
                 if item.name != "Sulfuras, Hand of Ragnaros":
                     item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
-                    if item.name != "Aged Brie":
+                    if item.name == "Aged Brie":
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    else:
                         if item.name != "Backstage passes to a TAFKAL80ETC concert":
                             if item.quality > 0:
                                 if item.name != "Sulfuras, Hand of Ragnaros":
                                     item.quality = item.quality - 1
                         else:
                             item.quality = item.quality - item.quality
-                    else:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+                        
 
 
 class Item:
