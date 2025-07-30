05eaac9e2246a1e28f393e122cd68e53d590a84d
Steven Solomon <solomon.steven.m@gmail.com>
2025-07-30 19:46:04 +0000

    Remove disued condition

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -21,8 +21,7 @@ class GildedRose(object):
             else:
                 if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                     if item.quality > 0:
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            item.quality = item.quality - 1
+                        item.quality = item.quality - 1
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
@@ -34,8 +33,9 @@ class GildedRose(object):
                                 if item.quality < 50:
                                     item.quality = item.quality + 1
 
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    item.sell_in = item.sell_in - 1
+                
+                item.sell_in = item.sell_in - 1
+
                 if item.sell_in < 0:
                     if item.name == "Aged Brie":
                         if item.quality < 50:
@@ -45,8 +45,7 @@ class GildedRose(object):
                             item.quality = item.quality - item.quality
                         else:
                             if item.quality > 0:
-                                if item.name != "Sulfuras, Hand of Ragnaros":
-                                    item.quality = item.quality - 1
+                                item.quality = item.quality - 1
                             
                         
 
