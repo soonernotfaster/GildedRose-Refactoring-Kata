d8accea2de9892c5860372b42b9d8adcc314b6a7
Steven Solomon <solomon.steven.m@gmail.com>
2025-07-30 19:41:55 +0000

    Invert condition for backstage pass

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -41,12 +41,13 @@ class GildedRose(object):
                         if item.quality < 50:
                             item.quality = item.quality + 1
                     else:
-                        if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                            item.quality = item.quality - item.quality
+                        else:
                             if item.quality > 0:
                                 if item.name != "Sulfuras, Hand of Ragnaros":
                                     item.quality = item.quality - 1
-                        else:
-                            item.quality = item.quality - item.quality
+                            
                         
 
 
